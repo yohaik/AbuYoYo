d304094a4434931da6129436ef4e6db46801e56f
Ido Harel <idohr1993@gmail.com>
2018-11-14 18:11:35 +0200

    singelton

---
+++ app/src/main/java/com/HaHa/abuyoyo/model/datasource/Firebase_DBManager.java
@@ -0,0 +1,15 @@
+package com.HaHa.abuyoyo.model.datasource;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.HaHa.abuyoyo.model.backend.Backend;
+import com.HaHa.abuyoyo.model.backend.BackendFactory;
+
+public class Firebase_DBManager implements Backend {
+
+    @Override
+    public void addRequest(ContentValues trip, Context context) {
+
+    }
+}
