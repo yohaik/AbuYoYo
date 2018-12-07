3c090db75bab80713a7da5753b3944b8fdc2cfd8
Yohanan Haik <haik@g.jct.ac.il>
2018-12-07 14:30:05 +0200

    WIP - design login + splash

--- app/src/main/java/com/HaHa/abuyoyo/LoginActivity.java
+++ app/src/main/java/com/HaHa/abuyoyo/LoginActivity.java
@@ -31,6 +31,7 @@ public class  LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Connect the objects for screen
